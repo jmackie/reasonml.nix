ad416f396aab38fa8d26112eb3fe3ea0a661f451
Jordan Mackie <jmackie@protonmail.com>
2019-08-30 16:32:06 +0100

    Pin reason-language-server src

--- reason-language-server/default.nix
+++ reason-language-server/default.nix
@@ -1,14 +1,18 @@
-{ fetchFromGitHub, buildDunePackage, ppx_tools_versioned
-, ocaml-migrate-parsetree, reason }:
-buildDunePackage rec {
+{ fetchgit, buildDunePackage, ppx_tools_versioned, ocaml-migrate-parsetree
+, reason }:
+let
+  reason-language-server-src = {
+    name = "jaredly-reason-language-server";
+  } // builtins.fromJSON (builtins.readFile ./reason-language-server-src.json);
+
+in buildDunePackage rec {
   pname = "reason-language-server";
   version = "1.7.1";
-  src = fetchFromGitHub {
-    owner = "jaredly";
-    repo = pname;
-    rev = version;
-    sha256 = "1yprrwf0zfvil0xhwnifr1cw2acyxqrf03dz62y67y2i1zrgk68i";
-  };
+  src = with reason-language-server-src;
+    fetchgit {
+      inherit name url rev sha256;
+      fetchSubmodules = false;
+    };
   buildInputs = [ ppx_tools_versioned ocaml-migrate-parsetree reason ];
   postInstall = ''
     ln -vs $out/bin/Bin $out/bin/reason-language-server
