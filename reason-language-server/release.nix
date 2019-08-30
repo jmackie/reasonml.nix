24d9072499614806a893a39dbf91a7bbde024eac
Jordan Mackie <jmackie@protonmail.com>
2019-08-30 15:33:15 +0100

    Try fetching a binary of reason-language-server

---
+++ reason-language-server/release.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl, unzip }:
+stdenv.mkDerivation rec {
+  name = "reason-language-server";
+  version = "1.7.1";
+  src = fetchurl {
+    url =
+      "https://github.com/jaredly/reason-language-server/releases/download/${version}/rls-linux.zip";
+    sha256 = "18bvadkql0r71q4a6ywcqz184sig770pps32vz9r99y5anj1cxhb";
+  };
+  buildInputs = [ unzip ];
+  buildCommand = ''
+    unzip $src
+    mkdir -p $out/bin
+    EXE=$out/bin/${name}
+    install -D -m555 rls-linux/${name} $EXE
+    chmod u+w $EXE
+    patchelf --interpreter ${stdenv.cc.bintools.dynamicLinker} $EXE
+    chmod u-w $EXE
+  '';
+}
